7ac7a140907fe21cc00984ec6e5d5d479f46ec79
Heidi Ganac Nicholson <heidiganac@gmail.com>
2022-12-18 14:47:40 -0600

    removed last item in array, logged value by storing in new variable

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -12,16 +12,20 @@ console.log(partsNeeded);
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 
-let suppleChanges = [3, 5, -6, 0, 7, 11];
+let supplyChanges = [3, 5, -6, 0, 7, 11];
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
 
-console.log(suppleChanges[1]);
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+
+let removedItem = supplyChanges.pop(); 
+    // creates variable and sets it to value of removed item in array
+console.log(removedItem); // logs value of removed item: 11
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
